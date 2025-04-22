3f42b021482b71091a5b317d23077795e58fcaa4
Andy Kreling <krelinga@gmail.com>
2025-04-22 21:43:39 +0000

    add a favicon.

--- render_sloth_icon.sh
+++ render_sloth_icon.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
 inkscape sloth.svg --export-type=png --export-filename=assets/images/masthead.png --export-width=88
+inkscape sloth.svg --export-type=png --export-filename=assets/images/favicon.png --export-width=64 --export-height=64
